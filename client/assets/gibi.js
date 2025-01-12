df35b835f35064ce4263309aa9655e51f9db3cf1
Gugguuuuu <gustavo2011oliveira.lima@gmail.com>
2025-01-12 16:16:53 -0300

    oi

--- client/assets/gibi.js
+++ client/assets/gibi.js
@@ -4,4 +4,4 @@ var buttonCheck = document.getElementById('check');
 buttonCheck.addEventListener('click', async () =>{
    var listaDeGibis = await fetch('http://localhost:3000/database');
    
-})
+})//
