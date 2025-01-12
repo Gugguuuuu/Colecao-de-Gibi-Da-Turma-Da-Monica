cbc06f5edc3e633226bbed907115cdc440366275
Gugguuuuu <gustavo2011oliveira.lima@gmail.com>
2025-01-12 15:26:06 -0300

    :tada: Commit inicial

---
+++ app.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var app = express()
+var mysql = require('mysql2');
+
+app.use(express.json());
+
+var con = mysql.createConnection({//
+  host: "localhost",
+  user: "root",
+  password: "Gustavo2011@#",
+  database: 'gibis'
+});
+
+con.connect((err) => {
+  if (err) {
+    console.error("Erro de conexão:", err);
+    return;
+  }
+  console.log("Conectado!");
+});
+
+
+function getGibi() { // ESSA FUNÇAO TEM Q SER ASSINCRONA
+  var cay = new Promise((resolve, reject) => { ///
+    con.query('select nome from nomes_de_gibi;', function (err, result, fields) {//
+
+      if (err) {
+        console.error("Erro de conexão:", err);
+        return reject(err);//
+      } else{
+        resolve(result)//
+      }
+    })
+  
+  })
+  return cay;
+}
+
+app.get('/', (req, res) => {
+  res.send('bom dia')
+})
+
+app.get('/database', async(req, res) => {///
+  try{
+      var resp = await getGibi();///
+      res.json(resp)//
+  } catch(err){
+      res.send(`Algo deu erradoooo! ${err.message}`)///
+  }
+})
+
+
+app.listen(3000, (err) => {
+  if (err) {
+    console.error('ERRO!', err.message)
+  } else { console.log('http://localhost:3000'); }
+});
---
+++ gibi.js
@@ -0,0 +1,7 @@
+var nomeDoGibi = document.getElementById('textGibi').value;
+var buttonCheck = document.getElementById('check');
+
+buttonCheck.addEventListener('click', async () =>{
+   var listaDeGibis = await fetch('http://localhost:3000/database');
+   
+})
